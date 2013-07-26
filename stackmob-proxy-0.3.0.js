c7148e4d838fe75762d48862a25a2dbe1d99a16a
Erick Tai <erick@stackmob.com>
2013-07-25 23:52:44 -0700

    cleaning things up

--- stackmob-proxy-0.3.0.js
+++ stackmob-proxy-0.3.0.js
@@ -5,6 +5,8 @@
  */
 _.extend(StackMob, {
 
+  hostedDomain: 'dev.proxyexperiment.tai.stackmobapp.com',
+
   /**
   Change this domain to the domain of your proxy.html
 
@@ -14,6 +16,8 @@ _.extend(StackMob, {
 
   initStart : function(options) {
     if(options['proxy']) {
+
+      //Auto add an iframe to hold the remote HTML page
       var frame = document.createElement('iframe');
       frame.setAttribute('src', options['proxy'] || throwError('No proxy frame URL provided.'));
       frame.setAttribute('id', 'theiframe');
@@ -30,73 +34,59 @@ _.extend(StackMob, {
 
       var dis = this;
 
+      //callback
+
       window.addEventListener('message', function(event) {
         if(event.origin == ("http://" + dis.hostedDomain)) {
 
           var payload = JSON.parse(event.data);
 
           var result = payload['result'];
-          var response = payload['response'];
+          var response = JSON.parse(payload['response']);
+          var statusCode = payload['statusCode'];
           var status = payload['status'];
           var call_id = payload['call_id'];
 
+
           if(result === 'success') {
-            StackMob['callLog'][call_id]['success'](response);
+            StackMob['callLog'][call_id]['success'](result, response, status);
           } else {
-            StackMob['callLog'][call_id]['error'](StackMob['callLog'][call_id]['model'], response);
+            StackMob['callLog'][call_id]['error'](result, response, status, statusCode);
           }
 
           //Cleanup
-          StackMob['callLog'][call_id]['success'] = null;
-          StackMob['callLog'][call_id]['error'] = null;
-          StackMob['callLog'][call_id]['model'] = null;
+          delete StackMob['callLog'][call_id]['success'];
+          delete StackMob['callLog'][call_id]['error'];
+          delete StackMob['callLog'][call_id]['model'];
+          delete StackMob['callLog'][call_id]['options'];
           delete StackMob['callLog'][call_id];
         }
       }, false);
     }
   },
+
   callLog : {},
 
-  'ajax' : function(model, params, method) {
+  'ajax' : function(model, params, method, options) {
     var originalSuccess = params['success'];
     var originalError = params['error'];
     
-    var wrappedSuccess = function(response, status, xhr) {
-      var result;
-
-      if(params["stackmob_count"] === true) {
-        result = xhr;
-      } else if(response && response.toJSON) {
-        result = response;
-      } else if(response && (response.responseText || response.text)) {
-        var result;
-
-        try {
-          result = JSON.parse(response.responseText || response.text);
-        } catch (e) {
-          result = response.responseText || response.text;
-        }
-      } else if(response) {
-        result = response;
-      }
-      StackMob.onsuccess(model, method, params, result, success, options);
-
+    var delayedSuccess = function(result, response, status) {
+      StackMob.onsuccess(model, method, params, response, originalSuccess, options);
     };
     
-    params['success'] = defaultSuccess;
+    params['success'] = delayedSuccess;
 
 
 
-    params['error'] = function(jqXHR, textStatus, errorThrown) {
-      // Workaround for Android broswers not recognizing HTTP status code 206.
-      // Call the success method on HTTP Status 0 (the bug) and when a range was specified.
-      if (jqXHR.status === 0 && params['query'] && (typeof params['query']['range'] === 'object')){
-        this.success(jqXHR, textStatus, errorThrown);
-        return;
-      }
-      var responseText = jqXHR.responseText || jqXHR.text;
-      StackMob.onerror(jqXHR, responseText, $.ajax, model, params, error, options);
+    var delayedError = function(result, response, status, statusCode) {
+      var jqXHR = { status: statusCode };
+      var responseAsString = JSON.stringify(response);
+      StackMob.onerror(jqXHR, responseAsString, $.ajax, model, params, originalError, options);
     };
+
+    params['error'] = delayedError;
+
 
     var call_id = (new Date()).getTime() + '_' + method;
 
@@ -106,9 +96,10 @@ _.extend(StackMob, {
 
     StackMob['callLog'][call_id] = {};
 
-    StackMob['callLog'][call_id]['success'] = defaultSuccess;
-    StackMob['callLog'][call_id]['error'] = defaultError;
+    StackMob['callLog'][call_id]['success'] = delayedSuccess;
+    StackMob['callLog'][call_id]['error'] = delayedError;
     StackMob['callLog'][call_id]['model'] = model;
+    StackMob['callLog'][call_id]['options'] = options;
 
     var payload = {
       'call_id' : call_id,
@@ -116,7 +107,8 @@ _.extend(StackMob, {
     };
 
     if(StackMob['proxyframe'])
-      StackMob['proxyframe'].contentWindow.postMessage(JSON.stringify(payload), ('http://' + this.hostedDomain));
+      StackMob['proxyframe'].contentWindow.postMessage(
+        JSON.stringify(payload), ('http://' + this.hostedDomain));
     else
       throwError('No proxy frame found.');
   }
